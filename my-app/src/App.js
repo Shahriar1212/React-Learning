7e07ac5dc8c942e630da97ce691acc6f046d49d0
Shahriar1212 <43474871+Shahriar1212@users.noreply.github.com>
2022-04-12 18:09:30 +0600

    nested mapping

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from './components/Card'
 import Data from './data.json'
+import Data1 from './data1.json'
 
 
 function App(){
@@ -17,7 +18,16 @@ function App(){
         <h1 className='headingStyle'>Todo App</h1>
 
         {Data.map((item,index) => <Card key={index} titleText = {item.title} descText={item.desc}/>)}
-        
+        {Data1.map((item,index) => <div key={index} className="card">
+            <h1>{item.name}</h1>
+            <h3>{item.age}</h3>
+            {
+              item.phone.map((phone,index) => <div>
+                <p>{phone.home}</p>
+                <p>{phone.office}</p>
+              </div>)
+            }
+        </div>)}
         {/* <Card titleText = {Data[0].title} descText={Data[0].desc}/>
         <Card titleText = {Data[1].title} descText={Data[1].desc}/>
         <Card titleText = {Data[2].title} descText={Data[2].desc}/> 
