dff147f72613c2538bf5f2bce78a3d6ce8a8804f
Shahriar1212 <43474871+Shahriar1212@users.noreply.github.com>
2022-04-12 13:43:51 +0600

    json data mapping

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import Card from './components/Card'
+import Data from './data.json'
 
 
 function App(){
+
+    let items = [];
+    // for(let x=0;x<Data.length;x++)
+    // {
+    //   items.push(<Card titleText = {Data[x].title} descText={Data[x].desc}/>)
+    // }
+
+    // items = Data.map((item) => <Card titleText = {item.title} descText={item.desc}/>)
+
     return <div>
         <h1 className='headingStyle'>Todo App</h1>
-        <Card titleText = "Call Mother" descText="this is a short description"/>
-        <Card titleText = "Call Father" descText="this is a short description"/>
-        <Card titleText = "Call Brother" descText="this is a short description"/>
-    </div> ;
+
+        {Data.map((item,index) => <Card key={index} titleText = {item.title} descText={item.desc}/>)}
+        
+        {/* <Card titleText = {Data[0].title} descText={Data[0].desc}/>
+        <Card titleText = {Data[1].title} descText={Data[1].desc}/>
+        <Card titleText = {Data[2].title} descText={Data[2].desc}/> 
+        <Card titleText = {Data[3].title} descText={Data[3].desc}/> */}
+      </div> ;
 }
 
 export default App;
