e34abccb4aaa7b3ee85b564499432bde267a4f1b
Shahriar1212 <43474871+Shahriar1212@users.noreply.github.com>
2022-04-13 14:47:58 +0600

    React state basic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,10 @@
-import React from 'react'
-import Card from './components/Card'
-import Data from './data.json'
-import Data1 from './data1.json'
-import Card1 from './components/Card1'
-import Card2 from './components/Card2'
+import React from "react";
+import State from './components/State'
 
-
-function App(){
-
-    return (
-      <div>
-        <Card1 name="Card1" desc="This is Card1"/>
-        <Card2 name="Card2" desc="Hello from Card2"/>
-      </div>
-      
-    )
+export default function App(){
+  return(
+    <div>
+      <State />
+    </div>
+  )
 }
-
-export default App;
